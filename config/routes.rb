b7a97cecbff9e2076728991f0b342c1aef5986af
Sudharshan <sudhar.ti@gmail.com>
2016-06-15 00:01:20 -0500

    Handle user defined exception

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users, only: [:create, :show]
+  get 'not_visible', to: 'home#not_visible'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -3,24 +3,24 @@ module Error
     def self.included(clazz)
       clazz.class_eval do
         rescue_from StandardError do |e|
-          respond(:internal_server_error, 500, e.to_s)
-        end
-        rescue_from CustomError do |e|
-          respond(e.error, e.status, e.message)
+          respond(:unprocessable_entity, 422, e.to_s)
         end
         rescue_from ActiveRecord::RecordNotFound do |e|
           respond(:record_not_found, 404, e.to_s)
+        end
+        rescue_from NotVisibleError do |e|
+          respond(e.error, e.status, e.message, not_visible_path)
         end
       end
     end
 
     private
 
-    def respond(_error, _status, _message)
+    def respond(_error, _status, _message, where_to_redirect = root_path)
       json = Helpers::Render.json(_error, _status, _message)
       respond_to do |format|
         format.json { render json: json }
-        format.html { redirect_to root_path, flash: { error: _message.to_s } }
+        format.html { redirect_to where_to_redirect, flash: { error: _message.to_s } }
       end
     end
   end
