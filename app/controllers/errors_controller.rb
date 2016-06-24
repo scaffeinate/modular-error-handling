2a67f4b5ef5d74c4b8d1b69e6b343ed53d4a3f77
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-06-24 02:23:12 -0500

    Update errors_controller.rb

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,27 +1,17 @@
 class ErrorsController < ApplicationController
   def not_found
-    respond_to do |format|
-      format.json do
-        render json: {
-          status: 404,
-          error: :not_found,
-          message: 'Where did the 403 errors go'
-        }, status: 404
-      end
-      format.html
-    end
+    render json: {
+      status: 404,
+      error: :not_found,
+      message: 'Where did the 403 errors go'
+    }, status: 404
   end
 
   def internal_server_error
-    respond_to do |format|
-      format.json do
-        render json: {
-          status: 500,
-          error: :internal_server_error,
-          message: 'Houston we have a problem'
-        }, status: 500
-      end
-      format.html
-    end
+    render json: {
+      status: 500,
+      error: :internal_server_error,
+      message: 'Houston we have a problem'
+    }, status: 500
   end
 end
