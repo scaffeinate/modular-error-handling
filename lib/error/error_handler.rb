183c8a8be52c813ef34296c2d5e5eb409292c59b
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-06-24 02:25:30 -0500

    Refactor ErrorHandler, Handle StandardError

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -2,26 +2,23 @@ module Error
   module ErrorHandler
     def self.included(clazz)
       clazz.class_eval do
-        rescue_from StandardError do |e|
-          respond(:unprocessable_entity, 422, e.to_s)
-        end
         rescue_from ActiveRecord::RecordNotFound do |e|
           respond(:record_not_found, 404, e.to_s)
         end
         rescue_from CustomError do |e|
-          respond(e.error, e.status, e.message, e.where_to_redirect)
+          respond(e.error, e.status, e.message.to_s)
+        end
+        rescue_from StandardError do |e|
+          respond(:standard_error, 500, e.to_s)
         end
       end
     end
 
     private
 
-    def respond(_error, _status, _message, where_to_redirect = nil)
+    def respond(_error, _status, _message)
       json = Helpers::Render.json(_error, _status, _message)
-      respond_to do |format|
-        format.json { render json: json, status: _status }
-        format.html { redirect_to where_to_redirect || root_path, flash: { error: _message.to_s } }
-      end
+      render json: json, status: _status
     end
   end
 end
