6787e25505e8579cbe1c8afe08cb717543bbd3e6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-06-24 02:24:31 -0500

    Update custom_error.rb

--- lib/error/custom_error.rb
+++ lib/error/custom_error.rb
@@ -1,12 +1,11 @@
 module Error
   class CustomError < StandardError
-    attr_reader :status, :error, :message, :where_to_redirect
+    attr_reader :status, :error, :message
 
-    def initialize(_error = nil, _status = nil, _message = nil, _where_to_redirect = nil)
+    def initialize(_error = nil, _status = nil, _message = nil)
       @error = _error || 422
       @status = _status || :unprocessable_entity
       @message = _message || 'Something went wrong'
-      @where_to_redirect = _where_to_redirect || :root
     end
 
     def fetch_json
